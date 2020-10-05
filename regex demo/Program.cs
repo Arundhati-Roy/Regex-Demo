e576d6002e147a252f66039c65a1842facc21165
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-10-05 09:14:27 +0530

    Pin Code Validation Added

--- regex demo/Program.cs
+++ regex demo/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Rewrite.Internal;
+using Microsoft.Extensions.FileSystemGlobbing;
+using System;
+using System.Text.RegularExpressions;
 
 namespace regex_demo
 {
@@ -7,6 +10,24 @@ namespace regex_demo
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to regex demo");
+
+            // Step 1: the input string.
+            Console.WriteLine("Pin code");
+            string pin = Console.ReadLine();
+            Regex regex = new Regex(@"^[1-9]{1}[0-9]{5}$");
+            
+            // Step 2: call Match on Regex instance.
+            Match match = regex.Match(pin);
+
+            // Step 3: test for Success.
+            if (match.Success)
+            {
+                Console.WriteLine("Pin code: " + match.Value);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Pin Code");
+            }
         }
     }
 }
