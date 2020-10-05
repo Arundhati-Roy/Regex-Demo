4169d2842e3583ab9765a0622e6065841d64794a
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-10-05 11:35:21 +0530

    Email Validation Features Added

--- regex demo/Program.cs
+++ regex demo/Program.cs
@@ -14,8 +14,9 @@ namespace regex_demo
             // Step 1: the input string.
             Console.WriteLine("Email");
             string pin = Console.ReadLine();
-            Regex regex = new Regex(@"^[^@\s]+@bridgelabz+\.+co+[^@\s]+$");
-            
+            //Regex regex = new Regex(@"^[^@\s.+_-]+@[^@\s]+\.+[^@\s]+$");
+            Regex regex = new Regex(@"^[a-z][a-zA-Z0-9.+_-][a-zA-Z0-9]+@[a-zA0-9]+\.+[^@\s]+$");
+            //Regex regex = new Regex(@"^([a-z][a-z0-9.+_-]*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?)$");
             // Step 2: call Match on Regex instance.
             Match match = regex.Match(pin);
 
