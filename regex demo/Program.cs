6bda32f31d57cee4a29eb039e2346d413ab5c5b0
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-10-06 09:41:57 +0530

    User Registration Added

--- regex demo/Program.cs
+++ regex demo/Program.cs
@@ -12,23 +12,50 @@ namespace regex_demo
             Console.WriteLine("Welcome to regex demo");
 
             // Step 1: the input string.
-            Console.WriteLine("Email");
+            Console.WriteLine("First Name");
             string pin = Console.ReadLine();
-            //Regex regex = new Regex(@"^[^@\s.+_-]+@[^@\s]+\.+[^@\s]+$");
-            Regex regex = new Regex(@"^[a-z][a-zA-Z0-9.+_-]+@[a-zA0-9]+\.(\.?[a-z]{2,}){1,2}$");
-            //Regex regex = new Regex(@"^([a-z][a-z0-9.+_-]*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?)$");
-            // Step 2: call Match on Regex instance.
-            Match match = regex.Match(pin);
-
-            // Step 3: test for Success.
-            if (match.Success)
-            {
-                Console.WriteLine("Email: " + match.Value);
-            }
+            Regex fname = new Regex(@"^[A-Z]{1}[a-z]{2,}$");
+            Match fnmatch = fname.Match(pin);
+            if (fnmatch.Success)
+                Console.WriteLine("First Name: " + fnmatch.Value);
             else
-            {
+                Console.WriteLine("Invalid First Name");
+
+            Console.WriteLine("Last Name");
+            string ln = Console.ReadLine();
+            Regex lname = new Regex(@"^[A-Z]{1}[a-z]{2,}$");
+            Match lnmatch = lname.Match(ln);           
+            if (lnmatch.Success)
+                Console.WriteLine("Last Name: " + lnmatch.Value);
+            else
+                Console.WriteLine("Invalid Last Name");
+
+            Console.WriteLine("Email");
+            string email = Console.ReadLine();
+            Regex regex = new Regex(@"^[a-z][a-zA-Z0-9.+_-]+@[a-zA0-9]+\.(\.?[a-z]{2,}){1,2}$");
+            Match match = regex.Match(email);
+            if (match.Success)
+                Console.WriteLine("Email: " + match.Value);
+            else
                 Console.WriteLine("Invalid Email");
-            }
+
+            Console.WriteLine("Phone no.");
+            string phno = Console.ReadLine();
+            Regex pn = new Regex(@"^[0-9]{1,3}\s[0-9]{10}$");
+            Match pnmatch = regex.Match(phno);
+            if (pnmatch.Success)
+                Console.WriteLine("No.: " + match.Value);
+            else
+                Console.WriteLine("Invalid Phone no.");
+
+            Console.WriteLine("Password");
+            string pass = Console.ReadLine();
+            Regex passc = new Regex(@"^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$");
+            Match pmatch = regex.Match(pass);
+            if (pmatch.Success)
+                Console.WriteLine("Password: " + match.Value);
+            else
+                Console.WriteLine("Invalid Passcode");
         }
     }
 }
